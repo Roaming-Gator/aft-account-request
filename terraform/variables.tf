fdeb3aa1e823b44729ade8f8d355712af6f2be32
Paul Nickerson <pvnick>
2023-04-15 19:15:06 -0400

    notes

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,3 @@
 variable "account_email_template" {
-  description = "Jinja template for account email. Should replace account_name jinja variable."
+  description = "Jinja template for account email, just to keep my email private. Should replace account_name jinja variable."
 }
