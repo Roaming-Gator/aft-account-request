ca4a3b53a28e6f9f26aaf4b1c167ff14f93e9351
Paul Nickerson <pvnick>
2023-04-17 22:35:21 -0400

    add a grafana account

--- terraform/grafana.tf
+++ terraform/grafana.tf
@@ -2,36 +2,36 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-data "jinja_template" "slackgpt-prod-email" {
+data "jinja_template" "grafana-email" {
   template = var.account_email_template
   context {
     data = jsonencode({
-      account_name = "slackgpt-prod"
+      account_name = "grafana"
     })
     type = "json"
   }
 }
 
-module "slackgpt-prod" {
+module "grafana" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = data.jinja_template.slackgpt-prod-email.result
-    AccountName  = "slackgpt-prod"
+    AccountEmail = data.jinja_template.grafana-email.result
+    AccountName  = "grafana"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Deployments"
     # Syntax for nested OU
-    SSOUserEmail     = data.jinja_template.slackgpt-prod-email.result
+    SSOUserEmail     = data.jinja_template.grafana-email.result
     SSOUserFirstName = "Paul"
     SSOUserLastName  = "Nickerson"
   }
   change_management_parameters = {
     change_requested_by = "Paul Nickerson"
-    change_reason       = "Slackgpt prod deployment"
+    change_reason       = "Grafana deployment"
   }
   account_tags = {
     "environment" = "prod"
   }
 
-  account_customizations_name = "slackgpt"
+  account_customizations_name = "grafana"
 }
