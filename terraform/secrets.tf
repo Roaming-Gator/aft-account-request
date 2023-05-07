a85cc7916ec4172e44d277d56a556ca3d224871e
Paul Nickerson <pvnick>
2023-05-07 16:59:39 -0400

    delete grafana and vault accounts, just have a secrets account

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -2,36 +2,38 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-data "jinja_template" "vault-email" {
+data "jinja_template" "secrets-email" {
   template = var.account_email_template
   context {
     data = jsonencode({
-      account_name = "vault"
+      account_name = "secrets"
     })
     type = "json"
   }
 }
 
-module "vault" {
+# note to self: make sure to add this account to control tower as
+# a delegated admin
+module "secrets" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = data.jinja_template.vault-email.result
-    AccountName  = "vault"
+    AccountEmail = data.jinja_template.secrets-email.result
+    AccountName  = "secrets"
     # Syntax for top-level OU
-    ManagedOrganizationalUnit = "Deployments"
+    ManagedOrganizationalUnit = "Security"
     # Syntax for nested OU
-    SSOUserEmail     = data.jinja_template.vault-email.result
+    SSOUserEmail     = data.jinja_template.secrets-email.result
     SSOUserFirstName = "Paul"
     SSOUserLastName  = "Nickerson"
   }
   change_management_parameters = {
     change_requested_by = "Paul Nickerson"
-    change_reason       = "Hashicorp vault deployment"
+    change_reason       = "Cross-account secrets"
   }
   account_tags = {
     "environment" = "prod"
   }
 
-  account_customizations_name = "vault"
+  account_customizations_name = "secrets"
 }
